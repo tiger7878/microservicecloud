b2ed3ec96a52e5919b2f5144decfa891e1745fa3
tiger7878 <zhcw63@163.com>
2018-09-20 21:25:43 +0800

    指定Ribbon的负载算法

--- microservicecloud-consumer-dept-80/src/main/java/com/monkey/springcloud/config/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/monkey/springcloud/config/ConfigBean.java
@@ -1,5 +1,8 @@
 package com.monkey.springcloud.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RetryRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +21,13 @@ public class ConfigBean {
     @LoadBalanced //Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端的负载均衡的工具，此注解表示进行负载均衡
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule iRule(){
+        //指定Ribbon的算法，替代默认的轮询算法
+//        return new RandomRule();//随机算法
+        return new RetryRule();//重试
     }
 
 }
